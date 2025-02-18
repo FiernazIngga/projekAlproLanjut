2141e8faa8c13787c776061c5b3746a20cbb1830
Cantika <cantika20@gmail.com>
2025-02-18 10:43:53 +0700

    struct

--- yoi.cpp
+++ yoi.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+    struct User{
+        string username;
+        string password;
+    };
+    struct Person{
+        string nama;
+        string noHp;
+        string alamat;
+    };
+
 int main(){
     
+
 }
