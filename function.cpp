02c3e19cbcda30e2364c2342948057c93374117e
daniel-png <lauhenapessy24@gmail.com>
2025-02-20 09:28:07 +0700

    asd

--- function.cpp
+++ function.cpp
@@ -38,6 +38,28 @@ void registrasi(){
     }
 }
 
+void aksesAkun(string username){
+    int pilihan;
+    bool menu = true;
+    while (menu)
+    {
+        system("cls");
+        cout << "Selamat datang " << username << " selamat berbelanjaa" << endl;
+        cout << "Menu\n1. Lihat Barang dan Beli\n2. Keranjang\n3. Lihat Data\n4. Keluar\nIsikan pilian anda: ";
+        cin >> pilihan;
+        cin.ignore();
+        switch (pilihan)
+        {
+        case 1:
+            system("cls");
+            break;
+        default:
+            menu = 1;
+            break;
+        }
+    }
+}
+
 void login(){
     string username, password;
     bool loginSukses = false;
@@ -70,7 +92,6 @@ void lihatBrg(Barang barang[], int index, int jumlahBarang){
     cout << "Harga Barang: " << barang[index].hargaBarang << endl;
     cout << "Rating Barang: " << barang[index].ratingBarang << endl;
     cout << endl;
-    // Panggil fungsi lihatbarang secara rekursif untuk elemen berikutnya dalam array
     lihatBrg(barang, index + 1, jumlahBarang);
 }
 
@@ -80,7 +101,84 @@ void lihatbarang(){
     system("pause");
 }
 
+void lihatBrgUser(Barang barang[], int index, int jumlahBarang) {
+    if (index == jumlahBarang) {
+        return;
+    }
 
-void aksesAkun(string username){
+    if (index == 0) {
+        cout << left << setw(20) << "Nama Barang"
+             << setw(20) << "Kategori Barang"
+             << setw(25) << "Deskripsi Barang"
+             << setw(15) << "Lokasi Barang"
+             << setw(15) << "Harga Barang"
+             << setw(15) << "Rating Barang" << endl;
+        cout << "-------------------------------------------------------------" << endl;
+    }
+
+    cout << left << setw(20) << barang[index].namaBarang
+         << setw(20) << barang[index].kategoriBarang
+         << setw(25) << barang[index].deskripsiBarang
+         << setw(15) << barang[index].lokasiBarang
+         << setw(15) << barang[index].hargaBarang
+         << setw(15) << barang[index].ratingBarang << endl;
+
+    lihatBrgUser(barang, index + 1, jumlahBarang);
+}
+
+void lihatBrgFilter(int filter){
+    system("cls");
+    string namaBarang = "";
+    while (filter >= 1 && filter <= 4)
+    {
+        switch (filter)
+        {
+        case 1:
+            cout << "Masukkan nama barang : "; getline(cin, namaBarang);
+            for (int i = 0; i < jumlahBarang; i++)
+            {
+                if (barang[i].namaBarang == namaBarang)
+                {
+                    cout << "Nama Barang: " << barang[i].namaBarang << endl;
+                }
+            }
+            break;
+        case 2:
+
+            break;
+        }
+    }
     
+}
+
+void lihatBarangUser(){
+    int menu;
+    bool ulang = true;
+    while (ulang)
+    {
+        system("cls");
+        cout << "Menu \n1. Lihat Semua Barang\n2. Kategori\n3. Keluar\nMasukkan Pilihan Anda: ";
+        cin >> menu;
+        cin.ignore();
+        switch (menu)
+        {
+        case 1:
+            lihatBarangUser();
+            break;
+        case 2:
+            system("cls");
+            int pilihanFilter;
+            cout << "Pilih kategori: \n1. Nama Barang\n2. Kategori Barang\n3. Harga Barang\n4. Rating Barang\n5. Keluar";
+            cin >> pilihanFilter;
+            cin.ignore();
+            lihatBrgFilter(pilihanFilter);
+            break;
+        case 3:
+            ulang = 0;
+            break;
+        default:
+            ulang = 1;
+            break;
+        }
+    }
 }
